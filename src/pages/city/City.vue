82d20d47de9eb75048789b36efdf57200eab7631
eexiaox <eexiaox@163.com>
2018-09-05 16:05:45 +0800

    city-ajax

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
-        <city-list></city-list>
-        <city-alphabet></city-alphabet>
+        <city-list :cities="cities" :hot="hotCities"></city-list>
+        <city-alphabet :cities="cities"></city-alphabet>
     </div>
 </template>
 <script>
@@ -12,6 +12,7 @@ import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
 import CityAlphabet from './components/Alphabet'
+import axios from 'axios'
 
 export default {
     name:'City',
@@ -20,6 +21,30 @@ export default {
         CitySearch,
         CityList,
         CityAlphabet
+    },
+    data(){
+        return{
+            cities:{},
+            hotCities:[]
+        }
+    },
+    methods:{
+        getCityInfor(){
+            axios.get('/api/city.json')
+                .then(this.handleGetCityInfoSucc)
+        },
+        handleGetCityInfoSucc(res){
+            res = res.data
+            if(res.ret && res.data){
+                const data = res.data
+                this.cities = data.cities
+                this.hotCities = data.hotCities
+            }
+            console.log(res)
+        }
+    },
+    mounted(){
+        this.getCityInfor()
     }
 }
 </script>
