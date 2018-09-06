854fa3d13a903ec1303835a9f2dfe24d4ab4ab6d
eexiaox <eexiaox@163.com>
2018-09-06 13:57:12 +0800

    finish pages

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
     <!-- 显示的是当前路由地址所对应的内容 -->
-    <router-view/>
+    <!-- keep-alive标签是vue自带的，当router-view路由加载过一次之后，就把路由中的内容放在内存之中，再一次再进入这个路由之后，不需要再进行渲染这个组件（重新执行钩子函数），只需将内容拿出来显示即可 -->
+    <keep-alive>
+       <router-view/>
+    </keep-alive>
   </div>
 </template>
 
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -16,6 +16,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import {mapState} from 'vuex'
 
 export default {
     name:'home',
@@ -28,16 +29,20 @@ export default {
     },
     data (){
         return{
+            lastCity:'',
             swiperList:[],
             iconList:[],
             recommendList:[],
             weekendList:[]
         }
     },
+    computed:{
+        ...mapState(['city'])
+    },
     methods:{
         getHomeInfo(){
             // // 只有将一些静态文件放在 static 下，才能够被访问，别的浏览器上通过输入地址打开的时候，都会跳转至根目录。
-            axios.get('/api/index.json')
+            axios.get('/api/index.json?city=' + this.city)
                 .then(this.getHomeInfoSucc)
         },
         getHomeInfoSucc(res){
@@ -53,7 +58,14 @@ export default {
         }
     },
     mounted (){
+        this.lastCity = this.city
         this.getHomeInfo()
+    },
+    activated(){
+        if(this.lastCity !== this.city){
+            this.lastCity = this.city
+            this.getHomeInfo()
+        }
     }
 }
 </script>
