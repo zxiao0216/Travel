6a9e56908f073f834cb2d32b3eea00b5a8ebc9bb
eexiaox <eexiaox@163.com>
2018-09-06 17:29:56 +0800

    detail header

---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,92 @@
+<template>
+    <div>   
+        <router-link 
+        class="header-abs"
+        tag="div"
+        to="/"
+        v-show="showAbs"
+        >
+            <div class="iconfont header-abs-back">&#xe624;</div>
+        </router-link>
+        <div class="header-fixed" 
+            v-show="!showAbs"
+            :style="opacityStyle"
+            >
+            <router-link to="/">
+                <div class="iconfont header-fixed-back">&#xe624;</div>
+            </router-link>
+            景点详情
+        </div>
+    </div>
+</template>
+
+<script>
+// import CommonGallary from 'common/gallary/Gallary'
+
+export default {
+    name:'DetailHeader',
+    data(){
+        return{
+            showAbs:true,
+            opacityStyle:{
+                opacity:0
+            }
+        }
+    },
+    methods:{
+        handleScroll (){
+            const top = document.documentElement.scrollTop
+            if(top > 62 ){
+                this.showAbs = false
+                let opacity = top /140
+                // 下边的一条的意思是，opacity最大值只能为1
+                opacity = opacity > 1 ? 1 :opacity
+                this.opacityStyle ={
+                    opacity
+                }
+            }else{
+                this.showAbs = true
+            }
+        }
+    },
+    activated(){
+        window.addEventListener('scroll',this.handleScroll)
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+     @import '~styles/varibles.styl';
+    .header-abs
+        position :absolute
+        left :.2rem
+        top :.2rem
+        width :.8rem
+        height :.8rem
+        line-height :.8rem
+        border-radius :.4rem
+        text-align :center
+        background :rgba(0,0,0,.8)
+        .header-abs-back
+            color :#fff
+            font-size :.4rem
+    .header-fixed
+        top :0
+        left :0
+        right :0
+        position :fixed
+        height :$headerHeight
+        line-height :$headerHeight
+        text-align :center
+        background  :$bgColor
+        color :#fff
+        font-size :.32rem
+        .header-fixed-back
+            position :absolute
+            top :0
+            left :0
+            width :.64rem
+            text-align :center
+            font-size :.4rem
+            color :#fff
+</style>
