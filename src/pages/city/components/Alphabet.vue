51218598b1a5bfb9993574b15fb3fb5e4eb68925
eexiaox <eexiaox@163.com>
2018-09-06 09:49:26 +0800

    optimization code

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,10 +1,16 @@
 <template>
     <ul class="list">
-        <li class="item"
-            v-for="(item,key) of cities" 
-            :key="key"
+        <li 
+            class="item"
+            v-for="item of letters" 
+            :key="item"
+            :ref="item"
+            @click="handleLetterClick"
+            @touchstart="handleTouchStart"
+            @touchmove="handleTouchMove"
+            @touchend="handleTouchEnd"
         >
-        {{key}}
+        {{item}}
         </li>
         </ul>
 </template>
@@ -13,6 +19,53 @@ export default {
     name:'CityAlphabet',
     props:{
         cities:Object
+    },
+    computed:{
+        letters(){
+             const letters = []
+             for(let i in this.cities){
+                 letters.push(i)
+             }
+             return letters
+        } 
+    },
+    data(){
+        return{
+            // touchStatus  手指的滑动
+            touchStatus :false,
+            startY :0,
+            timer:null
+        }
+    },
+    updated(){
+        this.startY = this.$refs['A'][0].offsetTop
+    },
+    methods:{
+        handleLetterClick(e){
+            this.$emit('change',e.target.innerText)
+        },
+        handleTouchStart(){
+            this.touchStatus = true
+        },
+        // 手指上下滑动的频率太快了
+        handleTouchMove(e){
+            if(this.touchStatus){
+                // startY 是指A字母距离输入框下边的位置，touchY是指 顶部到手指滑动的地方之间的距离-79，79指的是头部加输入框的位置。20指的是每个字母自己的高度
+                if(this.timer){
+                    clearTimeout(this.timer)
+                }
+                const timer = setTimeout(() => {
+                    const touchY = e.touches[0].clientY -79
+                    const index = Math.floor((touchY - this.startY)/20)
+                    if(index >= 0 && index < this.letters.length){
+                        this.$emit('change',this.letters[index])
+                    }
+                },16)  //16ms检测一次再只能，能够大大降低手指滑动的频率
+            }
+        },
+        handleTouchEnd(){
+             this.touchStatus = false
+        },
     }
 }
 </script>
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -24,7 +24,11 @@
                 </div>
             </div>
         </div>  
-        <div class="area" v-for="(item,key) of cities" :key="key">
+        <div class="area" 
+            v-for="(item,key) of cities" 
+            :key="key"
+            :ref="key"
+            >
             <div class="title border-topbottom">{{key}}</div>
             <div class="item-list">
                 <div class="item border-bottom"
@@ -45,10 +49,20 @@ export default {
     name:'CityList',
     props:{
         hot:Array,
-        cities:Object
+        cities:Object,
+        letter:String
     },
     mounted(){
         this.scroll = new Bscroll(this.$refs.wrapper)
+    },
+    watch:{
+        letter (){
+            if(this.letter){
+                const element = this.$refs[this.letter][0]
+                this.scroll.scrollToElement(element)
+            }
+            console.log(this.letter)
+        }
     }
 }
 </script>
